1fd0ed9a496e24dd5486c10c62139e1a02822758
HDM <cocu7712@naver.com>
2022-05-30 22:54:47 +0900

    2022-05-30 ch 47 clear

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,11 +3,10 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class memberApp {
+public class MemberApp {
 
     public static void main(String[] args) {
 //        AppConfig appConfig = new AppConfig();
